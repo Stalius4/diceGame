132eeb2a00f761ecd23478efcfc5a2b05990dea0
Stalius4 <deivydasrimgaila@gmail.com>
2022-04-28 11:40:17 +0100

    rolling animations

--- app.js
+++ app.js
@@ -12,16 +12,25 @@ const content2 = document.querySelector(".wrap")
 
 
 
+
+
+
 rollBtn.addEventListener("click", () => {
     cube.style.display = "none";
     diceFace1.style.display ="grid"
+    let images = ['dice1.png','dice2.png','dice3.png','dice4.png','dice5.png','dice6.png',]
 
+    let cnt=0
+    let obj =setInterval(randomImg, 100)
+    
+    
+    function randomImg() {
+        let random = Math.floor(Math.random()*6);
+        diceFace1.src = images[random]
+        cnt++
+        if(cnt==40){
+            clearInterval(obj)
+    
+        }
+    }
 })
-
-let images = ['dice1.png','dice2.png','dice3.png','dice4.png','dice5.png','dice6.png',]
-
-
-setInterval(function(){
-    let random = Math.floor(Math.random()*6);
-    diceFace1.src = images[random]
-}, 300)
